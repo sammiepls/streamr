a35f96d723a57b1d5fac5aa5142ca1aadf86f657
Wai Kit <wkitsin94@hotmail.com>
2017-10-04 22:23:39 +0800

    solving redis shit1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,17 @@ class PagesController < ApplicationController
 
   def home
   	count = 130 
-  	y = [0..10]
+  	y = (1..5).to_a
   	y.each do |i|
   		VideoJob.perform_now(count)
   		count -= 1 
   	end
 
-  	categories =  $redis.get("categories")
-    if categories.nil?
-      categories = Category.all.to_json
-      $redis.set("categories", categories)
-    end
+  	videos =  $redis.get("categories")
+   #  if categories.nil?
+   #    categories = Category.all.to_json
+   #    $redis.set("categories", categories)
+   #  end
   	     
   end
 end
--- app/jobs/video_job.rb
+++ app/jobs/video_job.rb
@@ -4,6 +4,5 @@ class VideoJob < ApplicationJob
   def perform(count)
    videos = Yt::Collections::Videos.new
    @vid = videos.where(order: 'viewCount', q: 'dota2stream').take(count).last.id 
-   
   end
 end
