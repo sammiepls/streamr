f383ba14b26bdbb081f254d94b47ec57dc2fa234
Wee Jing Heng <wheng3@illinois.edu>
2017-10-04 19:51:17 +0800

    google login completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :logged_in?, :current_user
+
+  def logged_in?
+    session.has_key? :user_id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(uid: session[:user_id]) if logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,15 @@ Rails.application.routes.draw do
   get "/login", to: redirect("/auth/google_oauth2")
   get "/auth/google_oauth2/callback", to: "sessions#create"
   resource :session, only: [:create, :destroy]
+  get 'home/show'
+
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# get "/login", to: redirect("/auth/google_oauth2")
+	# get "/auth/google_oauth2/callback", to: "sessions#create"
+	get "/logout", to: "sessions#destroy"
+	get '/auth/failure', to: redirect('/')
+
+	# resource :session, only: [:create, :destroy]
+
+	# root to: "home#show"
 end
