793a165d18ecd14c7d13417f3ad953644497e972
Wee Jing Heng <wheng3@illinois.edu>
2017-10-05 11:00:33 +0800

    User can now login and logout with google account

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171004093850) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
