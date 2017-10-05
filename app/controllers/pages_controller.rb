1985077cbb7ac9f9700ba12f78669fb6f985a876
Sammie <samantha.lhe@gmail.com>
2017-10-05 18:04:48 +0800

    Prepare for pull from master

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,12 @@ class PagesController < ApplicationController
     # Testing Ahoy Vists
     ahoy.track_visit
     ahoy.track "btn-keep", title: "Keep playing"
+    @total_visits = Visit.all
+    @total_btn_keep = Ahoy::Event.distinct.count('visit_id')
+
     @messages = Message.all
-  	@params = []
+    
+    @params = []
   	@params << Video.last.vid_id
   	@params << Video.last.vid_duration
     @params << Video.last.channel_title
--- app/jobs/video_job.rb
+++ app/jobs/video_job.rb
@@ -5,15 +5,14 @@ class VideoJob < ApplicationJob
    count = rand(80..130)
    videos = Yt::Collections::Videos.new
    @vid = videos.where(order: 'viewCount', q: 'dota2stream').take(count).last
-   # byebug 
+   # byebug
    @id = @vid.id 
    @duration = @vid.duration/2
    @title = @vid.title
-   @copyright = @vid.infringes_copyright? 
+   @copyright = @vid.infringes_copyright?
    @channel_title = @vid.channel_title
    @channel_id = @vid.channel_id
-   Video.last.update(vid_id: @id, vid_duration: @duration, 
+   Video.last.update(vid_id: @id, vid_duration: @duration,
     vid_title: @title, vid_copy: @copyright, channel_title: @channel_title, channel_id: @channel_id)
-   # VideoJob.set(wait: 1.minutes).perform_later(count)
   end
 end
