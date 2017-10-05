b091fcbe961cc3988774f8f98773f17e4a1c8f06
Esther Leong Yuet Mei <estherleongyuetmei@Esthers-MacBook-Air.local>
2017-10-05 22:20:26 +0800

    WIP for Subscribe button using yt gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,15 @@ class UsersController < ApplicationController
   end
 
   def subscribe
-byebug
   #channel to subscribe
-    @channel_id = subscribe_params
+    @channel_id = subscribe_params[:channel_id]
   #subscriber
     @account = Yt::Account.new access_token:  current_user.oauth_token
+    byebug
   #subscribe!
-    @channel = Yt::Channel.new id: 'UCxO1tY8h1AhOz0T4ENwmpow', auth: @account
+    @channel = Yt::Channel.new id: @channel_id, auth: @account
     @channel.subscribe
-
+    # 'UCxO1tY8h1AhOz0T4ENwmpow'
   end
 
   def subscribe_params
