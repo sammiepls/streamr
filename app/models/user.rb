242a4d7122a6811eb4f8253966c44bb9038424e4
Wee Jing Heng <wheng3@illinois.edu>
2017-10-05 11:44:30 +0800

    Added more fields into User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
 		user.uid = auth.uid
 		user.name = auth.info.name
 		user.email = auth.info.email
+		user.image = auth.info.image
 		user.oauth_token = auth.credentials.token
+		user.oauth_refresh_token = auth.credentials.refresh_token
 		user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 		user.save!
 		end
