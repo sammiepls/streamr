d9264af73590c6b73243be4e2e462e78b4cd8292
Wee Jing Heng <wheng3@illinois.edu>
2017-10-05 17:56:44 +0800

    Able to get user's live streaming url now

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], 
-  { scope: 'email, profile, https://www.googleapis.com/auth/youtube' }
+  { scope: 'email, profile, youtube, youtube.readonly' }
 end
