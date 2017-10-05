d06bf6d10e1b0c265adc0591a6f77868272cfbfa
Wee Jing Heng <wheng3@illinois.edu>
2017-10-05 13:35:03 +0800

    linking user info to their youtube acct

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,22 @@ class SessionsController < ApplicationController
 
 	# GET /auth/google_oauth2/callback
 	def create
- byebug
 		auth_hash = request.env['omniauth.auth']
 		user = User.from_omniauth(request.env['omniauth.auth'])
-   	session[:user_id] = user.id
-   	flash[:success] = "Welcome, #{user.name}"
+   		session[:user_id] = user.id
+   		flash[:success] = "Welcome, #{user.name}"
 
-      if user.oauth_token
+      	if user.oauth_token
 		    user.update_token(auth_hash)
-		  end
+		end
 
 		    #  @account = Yt::Account.new access_token: user.token
 		    #  @channel = Yt::Channel.new id: @account.channel.id, auth: @account
-		     redirect_to root_path
+		redirect_to root_path
+		user = User.from_omniauth(request.env["omniauth.auth"])
+		session[:user_id] = user.uid
+		@account = Yt::Account.new access_token: user.oauth_token
+		redirect_to root_path
 	end
 
 	def destroy
