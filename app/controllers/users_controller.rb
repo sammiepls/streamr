d76c422e8077e2b7202fb0d06167845469e8d617
Esther Leong Yuet Mei <estherleongyuetmei@Esthers-MacBook-Air.local>
2017-10-05 16:44:11 +0800

    Partial completion for posting subscribe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
   def create
-    
+
     redirect_to root_path
+
+  end
+
+  def subscribe
+byebug
+  #channel to subscribe
+    @channel_id = subscribe_params
+  #subscriber
+    @account = Yt::Account.new access_token:  current_user.oauth_token
+  #subscribe!
+    @channel = Yt::Channel.new id: 'UCxO1tY8h1AhOz0T4ENwmpow', auth: @account
+    @channel.subscribe
+
+  end
+
+  def subscribe_params
+
+    params.permit(:channel_id)
 
   end
 
