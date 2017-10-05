dfda9ea0a0d4bd1fb25a92e246bc15bbf6844a88
Wee Jing Heng <wheng3@illinois.edu>
2017-10-05 18:41:14 +0800

    Able to get logged_in user's youtube live link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
 
 	# GET /auth/google_oauth2/callback
 	def create
+<<<<<<< HEAD
 		auth_hash = request.env['omniauth.auth']
 			user = User.from_omniauth(request.env['omniauth.auth'])
 				session[:user_id] = user.uid
@@ -17,6 +18,14 @@ class SessionsController < ApplicationController
 					else
 					 	redirect_to root_path
 				end
+=======
+		user = User.from_omniauth(request.env["omniauth.auth"])
+		session[:user_id] = user.uid
+		@account = Yt::Account.new access_token: user.oauth_token
+		@live_id = @account.channel.videos.where(type: 'video', eventType:'live').first.id
+		flash[:success] = "Welcome, #{user.name}"
+		redirect_to root_path
+>>>>>>> Able to get logged_in user's youtube live link
 	end
 
 	def destroy
