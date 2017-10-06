caccc8bc60004f62b19aa3eb36c738740825d60c
Wee Jing Heng <wheng3@illinois.edu>
2017-10-06 11:14:42 +0800

    Fixed conflicts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,14 @@ class SessionsController < ApplicationController
 	def create
 		auth_hash = request.env['omniauth.auth']
 		user = User.from_omniauth(request.env['omniauth.auth'])
+   		session[:user_id] = user.id
+   		flash[:success] = "Welcome, #{user.name}"
+
+      	if user.oauth_token
+		    user.update_token(auth_hash)
+		end
+		redirect_to root_path
+		user = User.from_omniauth(request.env["omniauth.auth"])
 		session[:user_id] = user.uid
 		@account = Yt::Account.new access_token: user.oauth_token
 		@live_id = @account.channel.videos.where(type: 'video', eventType:'live').first.id
