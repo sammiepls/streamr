f27705ae6414f423e100cea3e37717f7d99ff228
Nyall Livett <nyall@Xn3r-dHu.local>
2017-10-06 13:14:59 +0800

    chages in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20171005074135) do
-=======
 ActiveRecord::Schema.define(version: 20171006033637) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
