b8516a380d901ecec8af679231ebafb042c5de6f
Wai Kit <wkitsin94@hotmail.com>
2017-10-06 11:56:08 +0800

    using cron as background jobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,16 @@
 class PagesController < ApplicationController
 
   def home
+    # Video.create(vid_id: 'asd', vid_duration: 123123, vid_title: 'asdasd', vid_copy: 'false')
   	@params = []
   	@params << Video.last.vid_id 
   	@params << Video.last.vid_duration 
+    @params << Video.last.channel_title
+    @params << Video.last.channel_id 
   end
 
   def update_video
+    # video
   	@params = [] 
   	@params << Video.last.vid_id 
   	@params << Video.last.vid_duration 
@@ -18,5 +22,17 @@ class PagesController < ApplicationController
   	json_content = {data: @params}
   	render json: json_content
   end 
+
+  # def video 
+  #   count = rand(80..130)
+  #   videos = Yt::Collections::Videos.new
+  #   @vid = videos.where(order: 'viewCount', q: 'dota2stream').take(count).last
+  #   @id = @vid.id 
+  #   @duration = @vid.duration/2
+  #   @title = @vid.title
+  #   @copyright = @vid.infringes_copyright? 
+  #   Video.update(vid_id: @id, vid_duration: @duration, vid_title: @title, vid_copy: @copyright)
+  #   # VideoJob.set(wait: 1.minutes).perform_later(count)
+  # end 
 
 end
--- app/jobs/video_job.rb
+++ app/jobs/video_job.rb
@@ -1,15 +1,19 @@
 class VideoJob < ApplicationJob
   queue_as :default
 
-  def perform(count)
+  def perform
    count = rand(80..130)
    videos = Yt::Collections::Videos.new
    @vid = videos.where(order: 'viewCount', q: 'dota2stream').take(count).last
+   # byebug 
    @id = @vid.id 
-   @duration = @vid.duration/2 
+   @duration = @vid.duration/2
    @title = @vid.title
    @copyright = @vid.infringes_copyright? 
-   Video.update(vid_id: @id, vid_duration: @duration, vid_title: @title, vid_copy: @copyright)
-   VideoJob.set(wait: 1.minutes).perform_later(count)
+   @channel_title = @vid.channel_title
+   @channel_id = @vid.channel_id
+   Video.last.update(vid_id: @id, vid_duration: @duration, 
+    vid_title: @title, vid_copy: @copyright, channel_title: @channel_title, channel_id: @channel_id)
+   # VideoJob.set(wait: 1.minutes).perform_later(count)
   end
 end
