a9bb2aac9c3b29a7da88f538882b1927b88b3b0f
Nyall Livett <nyall@Xn3r-dHu.local>
2017-10-09 16:48:56 +0800

    changed the production adapter form redis to redis store for config/cable.yml. Fixed production.rb for action cable allowed request origins (removed the 's' for one of the strings)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://streamr-next.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://streamr-next.herokuapp.com', 'https://streamr-next.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'http://streamr-next.herokuapp.com', 'https://streamr-next.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
