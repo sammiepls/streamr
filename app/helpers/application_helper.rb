c5c1869c5ea946721f42e9f8f6e823727fd3db8d
Esther Leong Yuet Mei <estherleongyuetmei@Esthers-MacBook-Air.local>
2017-10-10 12:04:39 +0800

    Tweaked login logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    session.has_key? :user_id
+  end
+
+
 end
